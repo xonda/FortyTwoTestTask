9b34a0fc5f8f10061eb7d2ff3c5005d0a4240c52
xonda <yamabushi@ukr.net>
2015-11-04 16:49:07 +0200

    added first test

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,9 +1,22 @@
 from django.test import TestCase
 
+from django.core.urlresolvers import reverse
+from django.test import Client
+from .models import Contacts
+
 # Create your tests here.
 
+client = Client()
 
 class SomeTests(TestCase):
     def test_math(self):
         "put docstrings in your tests"
-        assert(2 + 2 == 5)
+        assert(2 + 2 == 4)
+
+class MainPageTest(TestCase):
+    def test_home_page(self):
+        """
+        test if home page returns 200 ok
+        """
+        response = client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
