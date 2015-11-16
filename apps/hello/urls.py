da8a0ba6723b2b1378a6df4ed6bfd67a33c66abd
xonda <yamabushi@ukr.net>
2015-11-16 19:52:25 +0200

    ticket:1 moved static to assets

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
+from .views import home
 
 from django.contrib import admin
 admin.autodiscover()
 
-from hello.views import home
 
 urlpatterns = patterns(
     '',
