64dfbfea23c3c4c96ec704eafa0565ed33f594d9
xonda <yamabushi@ukr.net>
2015-11-17 13:20:57 +0200

    ticket:1 fixed multiline

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -5,11 +5,11 @@ class Info(models.Model):
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=20)
     dob = models.DateField(blank=True)
-    bio = models.CharField(max_length=200, blank=True)
+    bio = models.TextField(max_length=200, blank=True)
     email = models.EmailField(blank=True)
     jabber = models.CharField(max_length=20, blank=True)
     skype = models.CharField(max_length=20, blank=True)
-    other = models.CharField(max_length=200, blank=True)
+    other = models.TextField(max_length=200, blank=True)
 
     def __unicode__(self):
         return self.name
