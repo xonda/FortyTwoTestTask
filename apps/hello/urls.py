68607861a87243217b5b88224417df80ac0cb0bb
xonda <yamabushi@ukr.net>
2015-11-20 03:17:50 +0200

    fixed urls.py

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -7,10 +7,5 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^$', home, name='home'),
-
 )
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -7,10 +7,6 @@ from apps.hello import urls
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(urls)),
 )
