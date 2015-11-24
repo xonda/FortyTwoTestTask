78b5d2cab8b2e053081a48f0b33dc5774f79ad23
xonda <yamabushi@ukr.net>
2015-11-25 01:52:24 +0200

    ticket:5 fixed test

--- apps/hello/forms.py
+++ apps/hello/forms.py
@@ -3,10 +3,14 @@ from .models import Info
 
 
 class InfoForm(forms.ModelForm):
-    other = forms.CharField(widget=forms.Textarea(attrs={'cols': '45', 'rows': '6'}), label='Other contacts', required=True)
-    bio = forms.CharField(widget=forms.Textarea(attrs={'cols': '45', 'rows': '6'}), label='Biography', required=True)
+    other = forms.CharField(widget=forms.Textarea(
+        attrs={'cols': '45', 'rows': '6'}),
+        label='Other contacts', required=True)
+    bio = forms.CharField(widget=forms.Textarea(
+        attrs={'cols': '45', 'rows': '6'}), label='Biography', required=True)
     surname = forms.CharField(label='Last name')
-    dob = forms.CharField(widget=forms.TextInput(attrs={'id': 'datepicker'}), label='Date of birth', required=True)
+    dob = forms.CharField(widget=forms.TextInput(
+        attrs={'id': 'datepicker'}), label='Date of birth', required=True)
     email = forms.EmailField(required=True)
     jabber = forms.CharField(required=True)
     skype = forms.CharField(required=True)
