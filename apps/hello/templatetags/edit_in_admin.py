593eb49d086cbf23d4f8cc9cdc36f2a1b0e87f76
xonda <yamabushi@ukr.net>
2015-12-09 03:07:42 +0200

    ticket:8 added admin edit tag

---
+++ apps/hello/templatetags/edit_in_admin.py
@@ -0,0 +1,22 @@
+from django import template
+from django.core import urlresolvers
+
+register = template.Library()
+
+
+@register.tag()
+def edit_in_admin(render, token):
+    try:
+        tag_name, obj = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError("%r tag requires a single argument" % token.contents.split()[0])
+    return EditInAdminNode(obj)
+
+
+class EditInAdminNode(template.Node):
+    def __init__(self, obj):
+        self.obj = template.Variable(obj)
+
+    def render(self, context):
+        obj_pk = self.obj.resolve(context).pk
+        return urlresolvers.reverse('admin:hello_info_change', args=(obj_pk,))
