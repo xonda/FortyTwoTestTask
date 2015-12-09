9f245ff4f2bbe9c7ece4af07706a1820c3213fef
xonda <yamabushi@ukr.net>
2015-12-09 12:36:07 +0200

    ticket:5 fixed middleware

--- apps/hello/middleware.py
+++ apps/hello/middleware.py
@@ -7,16 +7,17 @@ class SaveRequest(object):
         """
         Saves each request to db
         """
-        WebRequest(
-            host=request.get_host(),
-            path=request.path,
-            method=request.method,
-            user_agent=request.META.pop('HTTP_USER_AGENT', None),
-            get=None if not request.GET else json.dumps(request.GET),
-            post=json.dumps(request.POST),
-            is_secure=request.is_secure(),
-            is_ajax=request.is_ajax(),
-            user=request.user
-        ).save()
+        if not request.path == '/upd_requests':
+            WebRequest(
+                host=request.get_host(),
+                path=request.path,
+                method=request.method,
+                user_agent=request.META.pop('HTTP_USER_AGENT', None),
+                get=None if not request.GET else json.dumps(request.GET),
+                post=json.dumps(request.POST),
+                is_secure=request.is_secure(),
+                is_ajax=request.is_ajax(),
+                user=request.user
+            ).save()
 
         return None
