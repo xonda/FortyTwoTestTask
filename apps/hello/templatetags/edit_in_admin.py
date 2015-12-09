c06e3636b417877d24ce6f9f4e38c0a70684ec17
xonda <yamabushi@ukr.net>
2015-12-09 03:19:02 +0200

    ticket:8 fixed PEP8

--- apps/hello/templatetags/edit_in_admin.py
+++ apps/hello/templatetags/edit_in_admin.py
@@ -4,15 +4,6 @@ from django.core import urlresolvers
 register = template.Library()
 
 
-@register.tag()
-def edit_in_admin(render, token):
-    try:
-        tag_name, obj = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError("%r tag requires a single argument" % token.contents.split()[0])
-    return EditInAdminNode(obj)
-
-
 class EditInAdminNode(template.Node):
     def __init__(self, obj):
         self.obj = template.Variable(obj)
@@ -20,3 +11,13 @@ class EditInAdminNode(template.Node):
     def render(self, context):
         obj_pk = self.obj.resolve(context).pk
         return urlresolvers.reverse('admin:hello_info_change', args=(obj_pk,))
+
+
+@register.tag()
+def edit_in_admin(render, token):
+    try:
+        tag_name, obj = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError("%r tag requires a single argument"
+                                           % token.contents.split()[0])
+    return EditInAdminNode(obj)
