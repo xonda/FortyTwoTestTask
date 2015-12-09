b80829824a51ce8303b91f66c1157990b489a352
xonda <yamabushi@ukr.net>
2015-12-09 12:12:54 +0200

    ticket:8 fix

--- apps/hello/seltests.py
+++ apps/hello/seltests.py
@@ -3,15 +3,19 @@ from selenium import webdriver
 
 
 class RequestsPageTests(TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
     def test_requests_page_content(self):
         """
         Test content added with ajax
         """
-        driver = webdriver.Chrome()
-        driver.get("http://127.0.0.1:8000/requests")
-        self.assertIn('Requests', driver.title)
-        tbody = driver.find_element_by_tag_name("tbody")
+        self.driver.get("http://127.0.0.1:8000/requests")
+        self.assertIn('Requests', self.driver.title)
+        tbody = self.driver.find_element_by_tag_name("tbody")
         self.assertIn(' /requests ', tbody.text)
         self.assertIn(' GET ', tbody.text)
         self.assertIn(' {} ', tbody.text)
-        driver.close()
+
+    def tearDown(self):
+        self.driver.close()
