5ba4b3f63f99b1c248ffe3993870fda4c5d5bd4c
xonda <yamabushi@ukr.net>
2015-12-16 20:35:40 +0200

    fixtures added

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -24,7 +24,7 @@ class MainPageTest(TestCase):
         response = client.get(reverse('home'))
         self.assertIn('<div class="block">', response.content)
         self.assertIn('"/static/css/main.css"', response.content)
-        self.assertIn('<p>Contacts</p>', response.content)
+        self.assertIn('<h3>Contacts</h3>', response.content)
 
     def test_home_empty_db(self):
         """
