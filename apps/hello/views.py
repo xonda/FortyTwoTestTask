3e112f0a32d8588d9587e0b975510a3a1a44f0be
xonda <yamabushi@ukr.net>
2015-12-18 16:20:43 +0200

    ticket:3 fixed views

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -4,11 +4,17 @@ from django.core import serializers
 from .models import Info, WebRequest
 
 
+# def home(request):
+#     try:
+#         info = Info.objects.all()[0]
+#     except:
+#         info = "Unfortunately there are no records in database"
+#
+#     context = {'info': info}
+#     return render(request, 'home.html', context)
+
 def home(request):
-    try:
-        info = Info.objects.all()[0]
-    except:
-        info = "Unfortunately there are no records in database"
+    info = Info.objects.first() or None
 
     context = {'info': info}
     return render(request, 'home.html', context)
@@ -19,11 +25,12 @@ def requests(request):
 
 
 def upd_requests(request):
-    if request.is_ajax():
-        last_10_reqs = WebRequest.objects.all().order_by('-id')[:10]
-        if last_10_reqs:
-            data = serializers.serialize("json", last_10_reqs)
-            return HttpResponse(data, content_type='application/json')
-        else:
-            return HttpResponse('No records in database')
-    return HttpResponse('Not ajax request')
+    if not request.is_ajax():
+        return HttpResponse('Not ajax request')
+
+    last_10_reqs = WebRequest.objects.all().order_by('-id')[:10]
+    if last_10_reqs:
+        data = serializers.serialize("json", last_10_reqs)
+        return HttpResponse(data, content_type='application/json')
+
+    return HttpResponse('No records in database')
