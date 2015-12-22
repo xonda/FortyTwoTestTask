d03cb763536f2dcbe08b225da61cf53c2b1cbfc6
xonda <yamabushi@ukr.net>
2015-12-22 17:51:34 +0200

    ticket:5 fix form

--- apps/hello/forms.py
+++ apps/hello/forms.py
@@ -2,23 +2,6 @@ from django import forms
 from .models import Info
 
 
-# class InfoForm(forms.ModelForm):
-#     other = forms.CharField(widget=forms.Textarea(
-#         attrs={'cols': '45', 'rows': '6'}),
-#         label='Other contacts', required=True)
-#     bio = forms.CharField(widget=forms.Textarea(
-#         attrs={'cols': '45', 'rows': '6'}), label='Biography', required=True)
-#     surname = forms.CharField(label='Last name')
-#     dob = forms.CharField(widget=forms.TextInput(
-#         attrs={'id': 'datepicker'}), label='Date of birth', required=True)
-#     email = forms.EmailField(required=True)
-#     jabber = forms.CharField(required=True)
-#     skype = forms.CharField(required=True)
-#     photo = forms.ImageField(widget=forms.FileInput())
-#
-#     class Meta:
-#         model = Info
-
 class InfoForm(forms.ModelForm):
     other = forms.CharField(widget=forms.Textarea(
         attrs={'cols': '45', 'rows': '6'}),
@@ -27,7 +10,6 @@ class InfoForm(forms.ModelForm):
         attrs={'cols': '45', 'rows': '6'}), label='Biography')
     dob = forms.CharField(widget=forms.TextInput(
         attrs={'id': 'datepicker'}), label='Date of birth')
-
 
     class Meta:
         model = Info
